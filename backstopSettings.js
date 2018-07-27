1a5dc35e1e8e8525091033bdf60c621e1877ebbb
Walmyr <wlsf82@gmail.com>
2018-07-27 18:03:01 +0200

    create project

---
+++ backstopSettings.js
@@ -0,0 +1,45 @@
+const relativePathConfig = require('./relativePaths.js');
+const relativePaths = relativePathConfig.array;
+const scenarios = [];
+
+relativePaths.map(relativePath => {
+  scenarios.push({
+    label: relativePath,
+    url: `http://talkingabouttesting.com/${relativePath}`,
+    readySelector: "iframe",
+    delay: 3000,
+    hideSelectors: [
+      "iframe",
+      ".widget_eu_cookie_law_widget"
+    ],
+    misMatchThreshold : 0.1,
+    requireSameDimensions: false
+  });
+});
+
+module.exports = {
+  id: "talking-about-testing",
+  viewports: [
+    {
+      name: "desktop",
+      width: 1280,
+      height: 1024
+    }
+  ],
+  scenarios,
+  paths: {
+    bitmaps_reference: "backstop_data/bitmaps_reference",
+    bitmaps_test: "backstop_data/bitmaps_test",
+    html_report: "backstop_data/html_report"
+  },
+  report: ["browser"],
+  engine: "puppeteer",
+  engineOptions: {
+    waitTimeout: 5000,
+    args: ["--no-sandbox"]
+  },
+  asyncCaptureLimit: 5,
+  asyncCompareLimit: 50,
+  debug: false,
+  debugWindow: false
+};
---
+++ relativePaths.js
@@ -0,0 +1,11 @@
+const relativePathConfig = {};
+
+relativePathConfig.array = [
+  'sobre',
+  'servicos/mentoria-coaching',
+  'servicos/consultoria',
+  'servicos/desenvolvimento',
+  'entre-em-contato'
+];
+
+module.exports = relativePathConfig;
